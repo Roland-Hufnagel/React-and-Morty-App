800e3ee89213b2c95b72d48b5105f01bc34ba198
Roland-Hufnagel <roland.hufnagel@gmx.de>
2022-10-03 21:17:41 +0200

    fix bug localStorage reload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import styled from 'styled-components';
 import Header from './components/Header/Header';
-import Card from './components/Card/Card';
 import Footer from './components/Footer/Footer';
 import CardContainer from './components/CardContainer/CardContainer';
 import { useEffect, useState } from 'react';
@@ -13,48 +12,38 @@ function App() {
   const API = 'https://rickandmortyapi.com/api/character';
   const [cards, setCards] = useState([]);
   const [favorites, setFavorites] = useLocalStorage([], 'favorites');
-  //const [favorites, setFavorites] = useState(JSON.stringify(localStorage.getItem('favorites')) && []);
+
   async function fetchCharacters() {
     try {
       const response = await fetch(API);
       const result = await response.json();
       const newArrayWithBookmark = result.results.map((data) => {
-        return { ...data, id: data.id.toString(), bookmarked: favorites.includes(data.id) ? true : false };
+        // change id to String and add bookmarked-status
+        return { ...data, id: data.id.toString(), bookmarked: favorites.includes(data.id.toString()) ? true : false };
       });
       setCards(newArrayWithBookmark);
-      console.log(newArrayWithBookmark);
     } catch (error) {
       console.error(error.message);
     }
   }
-  // useEffect(() => {
-  //   localStorage.setItem('favorites', JSON.stringify(favorites));
-  // }, [favorites]);
-
+  //fetch characters once
   useEffect(() => {
     fetchCharacters();
   }, []);
 
-  // useEffect(() => {
-  //   const newArray = cards.filter((card) => {
-  //     return card.bookmarked;
-  //   });
-  //   setFavorites(newArray);
-  // }, [cards]);
-
-  function removeFavorite(id) {
-    const index = favorites.indexOf(id);
-    const newArray = favorites.map(fav=>fav);
-    newArray.splice(index, 1);  //Achtung: splice liefert als Returnwert das gelöschte Element zurück!!!
-    setFavorites(newArray);
-  }
   function addFavorite(id) {
     setFavorites([...favorites, id]);
   }
 
+  function removeFavorite(id) {
+    const index = favorites.indexOf(id);
+    const newArray = favorites.map((fav) => fav);
+    newArray.splice(index, 1); //Achtung: splice liefert als Returnwert das gelöschte Element (als Array) zurück!!!
+    setFavorites(newArray);
+  }
+
   function toggleBookmark(id) {
-    console.log('toggle', id, typeof favorites);
-    favorites.includes(id) ? removeFavorite(id) : addFavorite(id);
+    favorites.includes(id) ? removeFavorite(id) : addFavorite(id); //add or remove from favorites
     setCards(
       cards.map((card) => {
         if (card.id === id) {
@@ -69,11 +58,9 @@ function App() {
   return (
     <AppContainer>
       <Header />
-      {/* <CardContainer cards={cards} /> */}
       <Routes>
         <Route path="/" element={<CardContainer cards={cards} />} />
         <Route path="/details/:id" element={<DetailCard cards={cards} onBookmark={toggleBookmark} />} />
-
         <Route path="/random" element={<CardContainer cards={cards} />} />
         <Route path="/favourites" element={<CardContainer cards={cards} />} />
       </Routes>
